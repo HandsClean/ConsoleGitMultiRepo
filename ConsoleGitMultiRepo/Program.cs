96fb32912b4e166142b4d36b425dbe9a24f8b698
Артем Розанов <a.rozanov@dataphone-russia.com>
2020-10-19 18:36:27 +0300

    Add project files.

---
+++ ConsoleGitMultiRepo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleGitMultiRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!5");
+            Console.WriteLine("Hello World!6");
+            Console.WriteLine("testbranch");
+        }
+    }
+}
